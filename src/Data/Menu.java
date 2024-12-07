8b4f12d78a6f2036c9a042ac76be7f51fb521e99
717kb <139049983+717kb@users.noreply.github.com>
2024-12-07 20:06:22 +0100

    Add files via upload

---
+++ src/Data/Menu.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Data;
+
+
+
+/**
+ *
+ * @author am.machuca.2023
+ */
+public class Menu {
+    private int discount;
+    private IndividualProduct[] products;
+    
+    public void setDiscount(int discount) {
+        this.discount = /*aqui tenemos que poner algo que lo lea del fichero*/
+    }
+   
+    
+    public int getPrice(){
+        int i, total=0;
+        for (i=0;i<this.getNumProducts() ;i++){
+            total+=this.products[i].getPrice();
+        }
+        return total*=(100-this.discount)/100;/*descuento del 30, 100-30=70 el precio es el 70 del precio, entonces precio*0,7*/
+    }
+    
+    public IndividualProduct getProduct(int i){
+        return this.products[i];
+    }
+
+    public int getNumProducts() {
+        return this.products.length;
+    }
+    
+}
---
+++ src/Data/MenuCard.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Data;
+
+import Manager.Section;
+import java.util.List;
+
+/**
+ *
+ * @author am.machuca.2023
+ */
+public class MenuCard {
+    private List<MenuCardSection> sectionList;
+    
+    public Section getSection(int c){
+        
+    }
+    
+    public int getNumberOfSections(){
+        
+    }
+    
+    public MenuCard loadFromDisk(){
+        
+    }
+
+    public MenuCard(List<MenuCardSection> sectionList) {
+        this.sectionList = sectionList;
+    }
+}
