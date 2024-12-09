a322bb99fe0b2c42540a77e47355f0c43a0b8873
717kb <139049983+717kb@users.noreply.github.com>
2024-12-09 15:52:12 +0100

    Update Order.java

--- src/Data/Order.java
+++ src/Data/Order.java
@@ -2,9 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Data;
+package products;
 
-import Data.Product;
+import products.Product;
 import java.util.Iterator;
 import java.util.List;
 
