79c6c3bd16212323f1089e9211a1b4d048dc92fc
magzero0099 <110187910+magzero0099@users.noreply.github.com>
2024-12-17 00:38:28 +0100

    Modif Payment 171224

--- src/Screens/PaymentScreen.java
+++ src/Screens/PaymentScreen.java
@@ -5,9 +5,56 @@
 package Screens;
 
 /**
- *
+ * 17/12/24
  * @author Alfa
  */
 public class PaymentScreen {
     
+
+    @Override
+    public KioskScreen show(Context c) {
+
+        //Metodo principal de la calse, desde el cual se produce la totalidad del proceso de pago
+
+        SimpleKiosk k=c.getKiosk();
+        TranslatorManager t=c.getTranslator();
+        configureScreenButtons(k,t);
+        char response=k.waitEvent(30);
+        switch (response){
+            case 'A':           //modifiar pedido. Devolvemos pantalla de menu
+                return new MenuScreen(c);
+            case 'B':           //cancelar pedido
+             return new WelcomeScreen(c);
+            default:
+                return this;
+        }       
+
+
+
+
+
+
+    }
+
+    //Metodos privados auxiliares
+
+    private void configureScreenButtons(SimpleKiosk k, TranslatorManager t) {
+        /*k.clearScreen();
+        k.setMenuMode(2);
+        String optA=t.translate("Modificar pedido"); //añadimos strings de las opciones traducidos
+        String optB=t.translate("Cancelar pago");
+        k.setOption('A', optA);
+        k.setOption('B', optB);*/
+    }
+
+    private void writeOrderToFile(){
+
+
+    }
+
+    private int incrementOrderNumber(){
+
+
+    }
+
 }
