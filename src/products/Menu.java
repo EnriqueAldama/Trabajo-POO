319f1e0afaa84183dcec3876f0a5ea04e20da719
magzero0099 <magzero0099@gmail.com>
2025-01-19 01:21:05 +0100

    discount gon menu

--- src/products/Menu.java
+++ src/products/Menu.java
@@ -4,10 +4,11 @@
  */
 package products;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
-
-
 
 /**
  *
@@ -16,42 +17,51 @@ import java.util.Iterator;
 public class Menu implements Product {
     private int discount;
     private ArrayList<IndividualProduct> prod;
-    
+
     public void setDiscount(int discount) {
-        this.discount = discount;
-    }
-   public Menu(){
-    this.prod=new ArrayList<IndividualProduct>();
-    this.discount=20;
-   }
-    
-    @Override
-    public int getPrice(){
-        int s=0; 
-        for (IndividualProduct p : this.prod) { 
-            s+=p.getPrice();
+        try (BufferedReader Buffr = new BufferedReader(new FileReader("COMANDAS\\discount"))) {
+
+            this.discount = (int) Integer.valueOf(Buffr.readLine());
+        } catch (IOException e) {
+            System.out.println("Fallo al leer fichero. Llamar a informatico");
         }
-        //return s=s*((100-this.discount)/100);/*descuento del 30, 100-30=70 el precio es el 70 del precio, entonces precio*0,7*/
-        return s=s*(100-this.discount)/100;
+
     }
-    
-    public IndividualProduct getProduct(int i){
+
+    public Menu() {
+        this.prod = new ArrayList<IndividualProduct>();
+        // this.discount=0;
+    }
+
+    @Override
+    public int getPrice() {
+        int s = 0;
+        for (IndividualProduct p : this.prod) {
+            s += p.getPrice();
+        }
+        // return s=s*((100-this.discount)/100);/*descuento del 30, 100-30=70 el precio
+        // es el 70 del precio, entonces precio*0,7*/
+        return s = s * (100 - this.discount) / 100;
+    }
+
+    public IndividualProduct getProduct(int i) {
         return this.prod.get(i);
     }
 
     public int getNumProducts() {
         return this.prod.size();
     }
-    
-    public void addIndProduct(IndividualProduct p){
-       this.prod.add(p);
+
+    public void addIndProduct(IndividualProduct p) {
+        this.prod.add(p);
     }
+
     @Override
     public String getName() {
-        String s= "Menu:";
-        for (int i=0;i<this.prod.size() ;i++){
-            s+= "1x "+this.getProduct(i).getName()+ " ";
+        String s = "Menu:";
+        for (int i = 0; i < this.prod.size(); i++) {
+            s += "1x " + this.getProduct(i).getName() + " ";
         }
-       return s;
+        return s;
     }
 }
